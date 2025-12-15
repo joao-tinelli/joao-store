5b92646b6c85e792523aac41d16d428b71016c42
Joao Tinelli <joao_tinelli@outlook.com>
2025-12-15 15:35:08 -0300

    Initial config

---
+++ joaostore/src/main/java/com/joaotinelli/joaostore/JoaostoreApplication.java
@@ -0,0 +1,13 @@
+package com.joaotinelli.joaostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaostoreApplication.class, args);
+	}
+
+}
