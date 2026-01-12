caba73ad721a9c870f46b8de7541414f2652f8a5
Joao Tinelli <joao_tinelli@outlook.com>
2026-01-12 15:47:14 -0300

    Add Dark Mode theme

--- joaostore-ui/src/components/footer/Footer.jsx
+++ joaostore-ui/src/components/footer/Footer.jsx
@@ -4,12 +4,12 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 export default function Footer(){
     return (
         <>
-        <footer className="flex justify-center items-center py-4 font-primary text-gray-700">
+        <footer className="flex justify-center items-center py-4 font-primary text-gray-700 dark:text-gray-300">
             Built with 
             <FontAwesomeIcon icon={faHeart} className="text-red-600 mx-1 animate-pulse" aria-hidden="true"/>
             by
             <a href="https://github.com/joao-tinelli" target="_blank" rel="noreferrer" 
-            className="text-primary font-semibold px-1 transition-colors duration-300 hover:text-dark">
+            className="text-primary dark:text-light font-semibold px-1 transition-colors duration-300 hover:text-dark dark:hover:text-lighter">
             Joao Tinelli
             </a>
         </footer>        
